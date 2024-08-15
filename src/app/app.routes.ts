fcb035fef0d82da186c52ddb76ee697f42592f2f
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-08-15 18:17:08 +0200

    Crear la ruta para Products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products', loadChildren: () => import('./products/features/product-shell/product.route'),
+  },
+  {
+    path: '', redirectTo: 'products', pathMatch: 'full',
+  }
+];
---
+++ src/app/products/features/product-shell/product.route.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+export default [
+  {path: '', loadComponent: () => import('../products-list/products-list.component')}
+] as Routes;
