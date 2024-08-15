55913b0d13e768b8c6e5e78305aba01c3bfc3892
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-08-15 18:44:18 +0200

    aplicar tipado en el servicio

--- src/app/products/data-access/products.service.ts
+++ src/app/products/data-access/products.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
 import { BaseHttpService } from '../../shared/data-access/base-http.service';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Injectable()
 export class ProductsService extends BaseHttpService{
-  getProducts() {
-    return this.http.get(`${this.apiUrl}/products`);
+  getProducts(): Observable<Product[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/products`);
   }
 }
