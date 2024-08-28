06bb684e97c877c163c3775d5a568fcee983cc67
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-08-28 11:15:14 +0200

    Pagina del carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
   {
     path: 'product', loadChildren: () => import('./products/features/product-shell/product.route'),
   },
+  { path: 'cart', loadChildren: () => import('./cart/cart.route') },
   { path: '',   redirectTo: '/product', pathMatch: 'full' },
   { path: '**', loadComponent: () => import('./core/components/page-not-found/page-not-found.component') },
 ];
