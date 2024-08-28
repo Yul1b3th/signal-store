981dbd3b68216c8e236c29c886ca5c95ccc0c249
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-08-28 12:48:56 +0200

    Cart item component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartItemComponent } from './ui/cart-item/cart-item.component';
+import { CartStateService } from '../shared/data-access/cart-state.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './cart.component.html',
   styles: ``
 })
 export default class CartComponent {
+  state = inject(CartStateService).state;
 
 }
